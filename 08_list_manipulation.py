b39e181822c523f6d989aa906adb3b09d592b31a
Bryan Pitman <pitman.bryan@gmail.com>
2022-07-25 17:15:05 -0700

    prob 8 complete

--- 08_list_manipulation.py
+++ 08_list_manipulation.py
@@ -40,3 +40,35 @@ def list_manipulation(lst, command, location, value=None):
         >>> list_manipulation(lst, 'add', 'dunno') is None
         True
     """
+    """if (command != "remove" or command != "add") or (location != 'beginning' or location != 'end'):
+        return None
+    elif command == "add" and location == "beginning":
+         lst.insert(0, value)
+         return lst
+    elif command == "add" and location == "end":
+        return lst.append(value)
+    elif command == "remove" and location == "beginning":
+        return lst.pop(0)
+    elif command == "remove" and location == "end":
+        return lst.pop()"""
+
+    if command == "remove":
+        if location == "beginning":
+            return lst.pop(0)
+        elif location == "end":
+            return lst.pop()
+        else:
+            return None
+
+    elif command == "add":
+        if location == "beginning":
+            lst.insert(0, value)
+            return lst
+        elif location == "end":
+            lst.append(value)
+            return lst
+        else:
+            return None
+
+    else:
+        return None
